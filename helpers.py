cfdce55833d26b315efe1db4acff245b28e9e886
KingLackland <faimireg@hotmail.com>
2020-02-26 09:55:56 +0100

    Initial commit

---
+++ helpers.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+
+def number_to_bitarray(array, modulo):
+    holder_x = []
+    mod_x = 0
+    max = 0
+    for card in array:
+        bits = 0
+        if modulo:
+            bits = 1 | bits
+            if (mod_x % 2) == 1:
+                max = 13
+            if (mod_x % 2) == 0:
+                max = 4
+            mod_x += 1
+        if not modulo:
+            if card == 0:
+                bits = 0
+            elif card > 0:
+                bits = 1 | bits
+            max = 10
+
+        card = max - card
+        try:
+            bits = bits << card
+        except:
+            print("bits: ", bits, "card: ", card, "max: ", max)
+
+        bits = bin(bits)[2:].zfill(max)
+
+        for bit in bits:
+            holder_x.append(int(bit))
+
+    return holder_x
+
+
+def loaddata(location, bits):
+    # load datasets
+    learning_set = open(location, 'r')
+    lines = learning_set.readline()
+    x = []
+    y = []
+
+    once = False
+    while lines:
+        container = list(map(int, lines.split(",")))
+
+        if(bits):
+            x.append(number_to_bitarray(container[:-1], True))
+            y.append(number_to_bitarray(container[-1:], False))
+
+        if not bits:
+            x.append(container[:-1])
+            y.append(container[-1:])
+
+        lines = learning_set.readline()
+
+    x = np.array(x)
+    y = np.array(y)
+
+    return x, y
+
+
+def split_list(a_list):
+    half = len(a_list) // 2
+    return a_list[:half], a_list[half:]
---
+++ trainingmodel.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from helpers import *
+
+x_train, y_train = loaddata('D:/Datasets/Poker/poker-hand-training-true.data', True)
+x_test, y_test = loaddata('D:/Datasets/Poker/poker-hand-testing.data', True)
+
+print("array_x: ", x_train, "size: ", x_train[0].__len__(), "array_y: ", y_train, "size: ", y_train[0].__len__())
+
+inputs = keras.Input(shape=(85,), name="hand")
+hiddenlayer = layers.Dense(18, activation='sigmoid', name="dense1")(inputs)
+hiddenlayer = layers.Dense(10, activation='sigmoid', name="dense2")(hiddenlayer)
+outputs = layers.Dense(10, name="prediction")(hiddenlayer)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+model.compile(optimizer=keras.optimizers.Adam(), loss='mean_squared_error')
+
+print('# Fit model on training data')
+history = model.fit(x_train, y_train,
+                    batch_size=32,
+                    epochs=10,
+                    # We pass some validation for
+                    # monitoring validation loss and metrics
+                    # at the end of each epoch
+                    validation_data=(x_test, y_test))
+
+print('\nhistory dict:', history.history)
+
+# Evaluate the model on the test data using `evaluate`
+print('\n# Evaluate on test data')
+results = model.evaluate(x_test, y_test, batch_size=32)
+print('test loss, test acc:', results)
+model.save('poker_predictor.h5')
