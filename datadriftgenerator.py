0eff9704a4300c70f5d0b167abdccb4215f928fd
KingLackland <faimireg@hotmail.com>
2020-02-27 15:47:19 +0100

    Incremental drift and progress bar for generating data

--- datadriftgenerator.py
+++ datadriftgenerator.py
@@ -1,9 +1,11 @@
+import time
+
 from helpers import *
 import numpy as np
 import statistics
 import random
-from progress.bar import Bar
-
+from tqdm import tqdm
+import threading
 
 x_test, y_test = loaddata('D:/Datasets/Poker/poker-hand-testing.data', False)
 print("array_x: ", x_test, "size: ", x_test[0].__len__(), "array_y: ", y_test, "size: ", y_test[0].__len__())
@@ -17,20 +19,17 @@ def categorical(datax, datay, labels):
         for p in range(len(rowx)):
             window[rowy][0][0][p].append(rowx[p])
 
-    bar = Bar('calculating mean and standard deviation', max=len(window))
-
     array_mean = []
     array_std = []
 
-    for i in range(len(window)):
-        bar.next()
+    print("creating mean and standard deviation")
+    for i in tqdm(range(len(window))):
         array_mean.append(np.mean(window[i][0], axis=1))
         array_std.append(np.std(window[i][0], axis=1))
 
     array_mean = np.array(array_mean)
     array_std = np.array(array_std)
 
-    bar.finish()
     return array_mean, array_std
 
 
@@ -42,9 +41,9 @@ def chances(datay, labels, random):
     size = len(datay)
 
     chance_array = []
-    bar = Bar('calculating chances', max=len(window))
-    for i in range(len(window)):
-        bar.next()
+
+    print("creating probabilities per collumn")
+    for i in tqdm(range(len(window))):
         chance_array.append(window[i] / size)
 
     chance_array = np.array(chance_array)
@@ -52,17 +51,22 @@ def chances(datay, labels, random):
     if random:
         np.random.shuffle(chance_array)
 
-    bar.finish()
     return chance_array
 
 
 def generatedatadriftfile(mean, std, amount, chance, labels, type):
-    choices = np.random.choice(len(labels), amount, p=chance)
+    choices = []
+
+    print("generating labels with the probabilities")
+    for i in tqdm(range(amount)):
+        choices.append(np.random.choice(len(labels), p=chance))
+
     generated = []
-    bar = Bar('generating data drift', max=len(choices))
-    for e in choices:
-        label_means = mean[e]
-        label_stds = std[e]
+
+    print("generating {0} data drift".format(type))
+    for e in tqdm(range(len(choices))):
+        label_means = mean[choices[e]]
+        label_stds = std[choices[e]]
         modulus = 0
         column = []
         for p in range(len(label_means)):
@@ -78,7 +82,6 @@ def generatedatadriftfile(mean, std, amount, chance, labels, type):
                 value = int(value)
 
                 column.append(value)
-                modulus += 1
 
             if type == "gradual-change":
                 binomial = np.random.choice(2, 1, p=[0.5, 0.5])
@@ -96,19 +99,24 @@ def generatedatadriftfile(mean, std, amount, chance, labels, type):
                 value = int(value)
 
                 column.append(value)
-                modulus += 1
 
-        bar.next()
-        column.append(e)
+            if type == "incremental-change":
+                label_means[p] = label_means[p] + (0.0000025 * label_stds[p])
+                value = int(label_means[p])
+                column.append(value)
+
+            modulus += 1
+        column.append(choices[e])
         generated.append(column)
 
     generated = np.array(generated)
+    print("saving file...")
     np.savetxt('D:/Datasets/Poker/poker-' + type + '.data', generated, fmt='%i', delimiter=',')
-    bar.finish()
+    print("file saved")
 
 
 labels = [e for e in range(0, 10)]
 
 mean, std = categorical(x_test, y_test, labels)
 chance = chances(y_test, labels, False)
-generatedatadriftfile(mean, std, 1000000, chance, labels, "sudden-change")
+generatedatadriftfile(mean, std, 1000000, chance, labels, "incremental-change")
